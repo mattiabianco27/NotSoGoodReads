65e69da5296cd8a67a4776584dbd17ee41c5bece
ElzPad <67328387+ElzPad@users.noreply.github.com>
2022-10-12 01:14:44 +0200

    Modified/Added JavaScript logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,44 @@
 const form = document.querySelector('#searchBar');
-const select = document.querySelector('#bookSelector');
+const bookQuery = document.querySelector('#bookQuery');
 const display = document.querySelector('#bookDisplay');
+
+const holder = {
+    text: [
+        "Search your book here",
+        "What are you waiting for?",
+    ],
+    tIndex: 0,
+    cIndex: 0
+};
+
 
 form.addEventListener('submit', e => {
     e.preventDefault();
     getBook(e.target.bookTitle.value);
+    e.target.reset();
 });
 
 select.addEventListener('click', e => {
-    if (e.target.classList.contains('saveBook')) {
-        console.log('Trying to add some new book to your collection... Will be available soon');
+    if (e.target.classList.contains('selectBook')) {
+        addBook(e.target);
     }
 });
+
+const interval = setInterval(async () => {
+    bookQuery.placeholder = holder.text[holder.tIndex].slice(0,holder.cIndex+1);
+
+    holder.cIndex++;
+
+    if (holder.tIndex === holder.text.length-1 && holder.cIndex === holder.text[holder.tIndex].length)
+        clearInterval(interval);
+    
+    if (holder.cIndex === holder.text[holder.tIndex].length) {    
+        await sleep(2000);
+        holder.tIndex = (holder.tIndex + 1)%holder.text.length;
+        holder.cIndex = 0;
+    }
+}, 70);
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
--- scripts/books.js
+++ scripts/books.js
@@ -1,3 +1,5 @@
+ const select = document.querySelector('#bookSelector');
+
 async function getBook(bookTitle) {
     const data = await requestBook(bookTitle);
     displayResults(data);
@@ -13,6 +15,27 @@ async function requestBook(bookTitle) {
     return data;
 }
 
+function addBook(bookElement) {
+    const title = bookElement.previousElementSibling.childNodes[1].innerText;
+    console.log(`Trying to add "${title}" to your books. This functionality will soon be available.`);
+}
+
 function displayResults(bookData) {
-    console.log(bookData);
+    select.innerHTML = "";
+    for (key in bookData.items) {
+        const bookInfo = bookData.items[key].volumeInfo;
+        const newDiv = document.createElement('div');
+
+        newDiv.classList.add('book');
+        newDiv.innerHTML = `
+            <img src="${bookInfo.imageLinks.thumbnail}">
+            <div class="bookInfo">
+                <p>${bookInfo.title}</p>
+                <p>${bookInfo.authors}</p>
+            </div>
+            <button type="button" class="selectBook">+</button>
+        `;
+
+        select.appendChild(newDiv);
+    }
 }
